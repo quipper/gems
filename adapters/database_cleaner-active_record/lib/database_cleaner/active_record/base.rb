c4238eec34b2fac460dd1b47ed8a3a030eb73ead
Irfan Fadilah <hello@irfanfadilah.com>
2025-11-18 13:25:57 +0700

    Support Rails 7.2

--- adapters/database_cleaner-active_record/lib/database_cleaner/active_record/base.rb
+++ adapters/database_cleaner-active_record/lib/database_cleaner/active_record/base.rb
@@ -67,8 +67,10 @@ module DatabaseCleaner
       end
 
       def self.migration_table_name
-        if ::ActiveRecord::VERSION::MAJOR < 5
-          ::ActiveRecord::Migrator.schema_migrations_table_name
+        if ::ActiveRecord::Base.connection_pool.respond_to?(:schema_migration) # Rails >= 7.2
+          ::ActiveRecord::Base.connection_pool.schema_migration.table_name
+        elsif ::ActiveRecord::Base.connection.respond_to?(:schema_migration) # Rails >= 6.0
+          ::ActiveRecord::Base.connection.schema_migration.table_name
         else
           ::ActiveRecord::SchemaMigration.table_name
         end
